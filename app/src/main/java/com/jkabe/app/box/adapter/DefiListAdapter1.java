00d411ab890e9387bf3c4a387b7d6606ed8e0301
aq610428 <641897605@qq.com>
2020-10-22 16:13:36 +0800

    Merge branch 'master' of http://github.com/aq610428/chinaBox
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/jkabe/app/box/adapter/AsetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/AssetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/IncomeAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/TraverAdapter.java
    #	app/src/main/java/com/jkabe/app/box/base/BaseApplication.java
    #	app/src/main/java/com/jkabe/app/box/bean/UserInfo.java
    #	app/src/main/java/com/jkabe/app/box/box/AssetsActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/DrawActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/IncomeActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/RechargeActivity.java
    #	app/src/main/java/com/jkabe/app/box/config/Api.java
    #	app/src/main/java/com/jkabe/app/box/config/okHttpModel.java
    #	app/src/main/java/com/jkabe/app/box/ui/ActivationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BindActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BrandCarActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/InvitationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LocationIndexActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LoginActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/MainActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/RegisterActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/UserActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/AssetsFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/CarFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MeFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MineFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MiningFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/util/Constants.java
    #	app/src/main/java/com/jkabe/app/box/util/FileManager.java
    #	app/src/main/java/com/jkabe/app/box/util/FileUtil.java
    #	app/src/main/java/com/jkabe/app/box/util/JsonParse.java
    #	app/src/main/java/com/jkabe/app/box/util/SaveUtils.java
    #	app/src/main/java/com/jkabe/app/box/util/SystemTools.java
    #	app/src/main/java/com/jkabe/app/box/weight/DialogUtils.java
    #	app/src/main/java/com/jkabe/app/box/weight/UpdateManager.java
    #	app/src/main/res/layout/activity_assets.xml
    #	app/src/main/res/layout/activity_bind.xml
    #	app/src/main/res/layout/activity_draw.xml
    #	app/src/main/res/layout/activity_income.xml
    #	app/src/main/res/layout/activity_location_map.xml
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_recharge.xml
    #	app/src/main/res/layout/custom_info_window.xml
    #	app/src/main/res/layout/fragment_assets.xml
    #	app/src/main/res/layout/fragment_me.xml
    #	app/src/main/res/layout/fragment_mining.xml
    #	app/src/main/res/layout/item_income.xml
    #	app/src/main/res/mipmap-xhdpi/icon_car.png
    #	app/src/main/res/mipmap-xxhdpi/icon_car.png
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    #	build.gradle
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/debug/R.jar
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/release/R.jar
    #	library1/build/intermediates/incremental/packageDebugResources/compile-file-map.properties
    #	library1/build/intermediates/incremental/packageReleaseResources/compile-file-map.properties
    #	library1/build/intermediates/manifest_merge_blame_file/debugAndroidTest/manifest-merger-blame-debug-androidTest-report.txt

---
+++ app/src/main/java/com/jkabe/app/box/adapter/DefiListAdapter1.java
@@ -0,0 +1,40 @@
+package com.jkabe.app.box.adapter;
+
+import android.content.Context;
+
+import com.jkabe.app.box.bean.OreInfo;
+import com.jkabe.box.R;
+
+import java.util.List;
+
+/**
+ * @author: zt
+ * @date: 2020/9/27
+ * @name:AddressAdapter
+ */
+public class DefiListAdapter1 extends AutoRVAdapter {
+    public List<OreInfo> list;
+    public Context context;
+
+    public DefiListAdapter1(Context context, List<OreInfo> list) {
+        super(context, list);
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int onCreateViewLayoutID(int viewType) {
+        return R.layout.item_defi_list1;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder vh, int position) {
+        OreInfo beans = list.get(position);
+
+
+    }
+
+    public void setData(List<OreInfo> beanList) {
+        this.list = beanList;
+    }
+}
--- app/src/main/java/com/jkabe/app/box/box/fragement/TabFragment4.java
+++ app/src/main/java/com/jkabe/app/box/box/fragement/TabFragment4.java
@@ -1,17 +1,23 @@
 package com.jkabe.app.box.box.fragement;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.aspsine.swipetoloadlayout.OnRefreshListener;
 import com.aspsine.swipetoloadlayout.SwipeToLoadLayout;
 import com.jkabe.app.box.adapter.DefiListAdapter;
+import com.jkabe.app.box.adapter.DefiListAdapter1;
 import com.jkabe.app.box.base.BaseFragment;
 import com.jkabe.app.box.bean.CommonalityModel;
 import com.jkabe.app.box.bean.OreInfo;
@@ -23,7 +29,9 @@ import com.jkabe.app.box.util.JsonParse;
 import com.jkabe.app.box.util.Md5Util;
 import com.jkabe.app.box.util.Utility;
 import com.jkabe.box.R;
+
 import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -33,12 +41,16 @@ import java.util.Map;
  * @date: 2020/10/20
  * @name:TabFragment1
  */
-public class TabFragment4 extends BaseFragment implements NetWorkListener, OnRefreshListener {
+public class TabFragment4 extends BaseFragment implements NetWorkListener, OnRefreshListener, View.OnClickListener {
     private View rootView;
     private RecyclerView recyclerView;
     private SwipeToLoadLayout swipeToLoadLayout;
     private List<OreInfo> beans = new ArrayList<>();
     private DefiListAdapter adapter;
+    private DefiListAdapter1 adapter1;
+    private LinearLayout ll_tab2, ll_tab1;
+    private TextView text_account, text_usdt, text_account_box, text_money_box;
+    private int page = 1;
 
 
     @Nullable
@@ -53,13 +65,55 @@ public class TabFragment4 extends BaseFragment implements NetWorkListener, OnRef
     }
 
     private void initView() {
+        text_money_box = getView(rootView, R.id.text_money_box);
+        text_account_box = getView(rootView, R.id.text_account_box);
+        text_usdt = getView(rootView, R.id.text_usdt);
+        text_account = getView(rootView, R.id.text_account);
+        ll_tab2 = getView(rootView, R.id.ll_tab2);
+        ll_tab1 = getView(rootView, R.id.ll_tab1);
         swipeToLoadLayout = getView(rootView, R.id.swipeToLoadLayout);
         recyclerView = getView(rootView, R.id.recyclerView);
         swipeToLoadLayout.setOnRefreshListener(this);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
+        ll_tab1.setOnClickListener(this);
+        ll_tab2.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_tab2:
+                clealView();
+                page = 2;
+                text_account_box.setTextColor(Color.parseColor("#FFFFFF"));
+                text_money_box.setTextColor(Color.parseColor("#FFFFFF"));
+                ll_tab2.setBackgroundResource(R.drawable.shape_bg_vx2);
+                query();
+                break;
+            case R.id.ll_tab1:
+                page = 1;
+                clealView();
+                text_account.setTextColor(Color.parseColor("#FFFFFF"));
+                text_usdt.setTextColor(Color.parseColor("#FFFFFF"));
+                ll_tab1.setBackgroundResource(R.drawable.shape_bg_vx2);
+                query();
+                break;
+        }
+    }
+
+    public void clealView() {
+        ll_tab1.setBackgroundResource(R.drawable.shape_bg_mi);
+        ll_tab2.setBackgroundResource(R.drawable.shape_bg_mi);
+        text_account.setTextColor(Color.parseColor("#333333"));
+        text_usdt.setTextColor(Color.parseColor("#F05330"));
+        text_account_box.setTextColor(Color.parseColor("#333333"));
+        text_money_box.setTextColor(Color.parseColor("#F05330"));
+    }
+
 
     @Override
     protected void lazyLoad() {
@@ -97,8 +151,13 @@ public class TabFragment4 extends BaseFragment implements NetWorkListener, OnRef
 
 
     private void setAdapter() {
-        adapter = new DefiListAdapter(getContext(), beans);
-        recyclerView.setAdapter(adapter);
+        if (page == 1) {
+            adapter = new DefiListAdapter(getContext(), beans);
+            recyclerView.setAdapter(adapter);
+        } else {
+            adapter1 = new DefiListAdapter1(getContext(), beans);
+            recyclerView.setAdapter(adapter1);
+        }
     }
 
 
@@ -118,4 +177,6 @@ public class TabFragment4 extends BaseFragment implements NetWorkListener, OnRef
     public void onRefresh() {
         query();
     }
+
+
 }
