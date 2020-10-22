54f96b794e335f219b84a773c250f25374e2d293
aq610428 <641897605@qq.com>
2020-10-22 11:07:26 +0800

    Merge branch 'master' of http://github.com/aq610428/chinaBox
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/jkabe/app/box/adapter/AsetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/AssetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/IncomeAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/TraverAdapter.java
    #	app/src/main/java/com/jkabe/app/box/base/BaseApplication.java
    #	app/src/main/java/com/jkabe/app/box/bean/UserInfo.java
    #	app/src/main/java/com/jkabe/app/box/box/AssetsActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/DrawActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/IncomeActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/RechargeActivity.java
    #	app/src/main/java/com/jkabe/app/box/config/Api.java
    #	app/src/main/java/com/jkabe/app/box/config/okHttpModel.java
    #	app/src/main/java/com/jkabe/app/box/ui/ActivationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BindActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BrandCarActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/InvitationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LocationIndexActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LoginActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/MainActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/RegisterActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/UserActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/AssetsFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/CarFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MeFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MineFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MiningFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/util/Constants.java
    #	app/src/main/java/com/jkabe/app/box/util/FileManager.java
    #	app/src/main/java/com/jkabe/app/box/util/FileUtil.java
    #	app/src/main/java/com/jkabe/app/box/util/JsonParse.java
    #	app/src/main/java/com/jkabe/app/box/util/SaveUtils.java
    #	app/src/main/java/com/jkabe/app/box/util/SystemTools.java
    #	app/src/main/java/com/jkabe/app/box/weight/DialogUtils.java
    #	app/src/main/java/com/jkabe/app/box/weight/UpdateManager.java
    #	app/src/main/res/layout/activity_assets.xml
    #	app/src/main/res/layout/activity_bind.xml
    #	app/src/main/res/layout/activity_draw.xml
    #	app/src/main/res/layout/activity_income.xml
    #	app/src/main/res/layout/activity_location_map.xml
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_recharge.xml
    #	app/src/main/res/layout/custom_info_window.xml
    #	app/src/main/res/layout/fragment_assets.xml
    #	app/src/main/res/layout/fragment_me.xml
    #	app/src/main/res/layout/fragment_mining.xml
    #	app/src/main/res/layout/item_income.xml
    #	app/src/main/res/mipmap-xhdpi/icon_car.png
    #	app/src/main/res/mipmap-xxhdpi/icon_car.png
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    #	build.gradle
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/debug/R.jar
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/release/R.jar
    #	library1/build/intermediates/incremental/packageDebugResources/compile-file-map.properties
    #	library1/build/intermediates/incremental/packageReleaseResources/compile-file-map.properties
    #	library1/build/intermediates/manifest_merge_blame_file/debugAndroidTest/manifest-merger-blame-debug-androidTest-report.txt

--- app/src/main/java/com/jkabe/app/box/box/fragement/TabFragment4.java
+++ app/src/main/java/com/jkabe/app/box/box/fragement/TabFragment4.java
@@ -1,23 +1,45 @@
 package com.jkabe.app.box.box.fragement;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.aspsine.swipetoloadlayout.OnRefreshListener;
+import com.aspsine.swipetoloadlayout.SwipeToLoadLayout;
+import com.jkabe.app.box.adapter.DefiListAdapter;
 import com.jkabe.app.box.base.BaseFragment;
+import com.jkabe.app.box.bean.CommonalityModel;
+import com.jkabe.app.box.bean.OreInfo;
+import com.jkabe.app.box.config.Api;
+import com.jkabe.app.box.config.NetWorkListener;
+import com.jkabe.app.box.config.okHttpModel;
+import com.jkabe.app.box.util.Constants;
+import com.jkabe.app.box.util.JsonParse;
+import com.jkabe.app.box.util.Md5Util;
+import com.jkabe.app.box.util.Utility;
 import com.jkabe.box.R;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author: zt
  * @date: 2020/10/20
  * @name:TabFragment1
  */
-public class TabFragment4 extends BaseFragment {
+public class TabFragment4 extends BaseFragment implements NetWorkListener, OnRefreshListener {
     private View rootView;
+    private RecyclerView recyclerView;
+    private SwipeToLoadLayout swipeToLoadLayout;
+    private List<OreInfo> beans = new ArrayList<>();
+    private DefiListAdapter adapter;
+
 
     @Nullable
     @Override
@@ -31,11 +53,69 @@ public class TabFragment4 extends BaseFragment {
     }
 
     private void initView() {
+        swipeToLoadLayout = getView(rootView, R.id.swipeToLoadLayout);
+        recyclerView = getView(rootView, R.id.recyclerView);
+        swipeToLoadLayout.setOnRefreshListener(this);
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 
     @Override
     protected void lazyLoad() {
+        query();
+    }
 
+
+    public void query() {
+        String sign = "partnerid=" + Constants.PARTNERID + Constants.SECREKEY;
+        Map<String, String> params = okHttpModel.getParams();
+        params.put("apptype", Constants.TYPE);
+        params.put("partnerid", Constants.PARTNERID);
+        params.put("sign", Md5Util.encode(sign));
+        okHttpModel.get(Api.GET_COINS_LIST, params, Api.GET_COINS_LIST_ID, this);
+    }
+
+
+    @Override
+    public void onSucceed(JSONObject object, int id, CommonalityModel commonality) {
+        if (object != null && commonality != null && !Utility.isEmpty(commonality.getStatusCode())) {
+            if (Constants.SUCESSCODE.equals(commonality.getStatusCode())) {
+                switch (id) {
+                    case Api.GET_COINS_LIST_ID:
+                        beans = JsonParse.getBespokemoniesJson1(object);
+                        if (beans != null && beans.size() > 0) {
+                            setAdapter();
+                        }
+                        break;
+                }
+            }
+        }
+        stopProgressDialog();
+        swipeToLoadLayout.setRefreshing(false);
+    }
+
+
+    private void setAdapter() {
+        adapter = new DefiListAdapter(getContext(), beans);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onFail() {
+        stopProgressDialog();
+        swipeToLoadLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onError(Exception e) {
+        stopProgressDialog();
+        swipeToLoadLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onRefresh() {
+        query();
     }
 }
