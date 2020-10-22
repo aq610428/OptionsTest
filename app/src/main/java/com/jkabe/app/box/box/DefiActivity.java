74046120dd0b8aca93239c64ec7e837b35b317cb
aq610428 <641897605@qq.com>
2020-10-22 10:20:55 +0800

    Merge branch 'master' of http://github.com/aq610428/chinaBox
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/jkabe/app/box/adapter/AsetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/AssetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/IncomeAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/TraverAdapter.java
    #	app/src/main/java/com/jkabe/app/box/base/BaseApplication.java
    #	app/src/main/java/com/jkabe/app/box/bean/UserInfo.java
    #	app/src/main/java/com/jkabe/app/box/box/AssetsActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/DrawActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/IncomeActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/RechargeActivity.java
    #	app/src/main/java/com/jkabe/app/box/config/Api.java
    #	app/src/main/java/com/jkabe/app/box/config/okHttpModel.java
    #	app/src/main/java/com/jkabe/app/box/ui/ActivationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BindActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BrandCarActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/InvitationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LocationIndexActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LoginActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/MainActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/RegisterActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/UserActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/AssetsFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/CarFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MeFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MineFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MiningFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/util/Constants.java
    #	app/src/main/java/com/jkabe/app/box/util/FileManager.java
    #	app/src/main/java/com/jkabe/app/box/util/FileUtil.java
    #	app/src/main/java/com/jkabe/app/box/util/JsonParse.java
    #	app/src/main/java/com/jkabe/app/box/util/SaveUtils.java
    #	app/src/main/java/com/jkabe/app/box/util/SystemTools.java
    #	app/src/main/java/com/jkabe/app/box/weight/DialogUtils.java
    #	app/src/main/java/com/jkabe/app/box/weight/UpdateManager.java
    #	app/src/main/res/layout/activity_assets.xml
    #	app/src/main/res/layout/activity_bind.xml
    #	app/src/main/res/layout/activity_draw.xml
    #	app/src/main/res/layout/activity_income.xml
    #	app/src/main/res/layout/activity_location_map.xml
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_recharge.xml
    #	app/src/main/res/layout/custom_info_window.xml
    #	app/src/main/res/layout/fragment_assets.xml
    #	app/src/main/res/layout/fragment_me.xml
    #	app/src/main/res/layout/fragment_mining.xml
    #	app/src/main/res/layout/item_income.xml
    #	app/src/main/res/mipmap-xhdpi/icon_car.png
    #	app/src/main/res/mipmap-xxhdpi/icon_car.png
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    #	build.gradle
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/debug/R.jar
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/release/R.jar
    #	library1/build/intermediates/incremental/packageDebugResources/compile-file-map.properties
    #	library1/build/intermediates/incremental/packageReleaseResources/compile-file-map.properties
    #	library1/build/intermediates/manifest_merge_blame_file/debugAndroidTest/manifest-merger-blame-debug-androidTest-report.txt

--- app/src/main/java/com/jkabe/app/box/box/DefiActivity.java
+++ app/src/main/java/com/jkabe/app/box/box/DefiActivity.java
@@ -5,9 +5,16 @@ import android.view.View;
 import android.widget.TextView;
 import com.jkabe.app.box.base.BaseActivity;
 import com.jkabe.app.box.bean.CommonalityModel;
+import com.jkabe.app.box.config.Api;
 import com.jkabe.app.box.config.NetWorkListener;
+import com.jkabe.app.box.config.okHttpModel;
+import com.jkabe.app.box.util.Constants;
+import com.jkabe.app.box.util.Md5Util;
+import com.jkabe.app.box.util.Utility;
+import com.jkabe.app.box.weight.ClearEditText;
 import com.jkabe.box.R;
 import org.json.JSONObject;
+import java.util.Map;
 
 /**
  * @author: zt
@@ -15,7 +22,8 @@ import org.json.JSONObject;
  * @name:DefiActivity
  */
 public class DefiActivity extends BaseActivity implements NetWorkListener {
-    private TextView title_text_tv, title_left_btn;
+    private TextView title_text_tv, title_left_btn,text_ensure;
+    private ClearEditText et_num;
 
 
     @Override
@@ -25,9 +33,12 @@ public class DefiActivity extends BaseActivity implements NetWorkListener {
 
     @Override
     protected void initView() {
+        et_num= getView(R.id.et_num);
         title_text_tv = getView(R.id.title_text_tv);
         title_left_btn = getView(R.id.title_left_btn);
+        text_ensure = getView(R.id.text_ensure);
         title_left_btn.setOnClickListener(this);
+        text_ensure.setOnClickListener(this);
         title_text_tv.setText("投保");
     }
 
@@ -39,6 +50,9 @@ public class DefiActivity extends BaseActivity implements NetWorkListener {
             case R.id.title_left_btn:
                 finish();
                 break;
+            case R.id.text_ensure:
+
+                break;
         }
     }
 
@@ -47,18 +61,38 @@ public class DefiActivity extends BaseActivity implements NetWorkListener {
 
     }
 
+
+
+    public void query() {
+        String sign = "partnerid=" + Constants.PARTNERID + Constants.SECREKEY;
+        Map<String, String> params = okHttpModel.getParams();
+        params.put("apptype", Constants.TYPE);
+        params.put("partnerid", Constants.PARTNERID);
+        params.put("sign", Md5Util.encode(sign));
+        okHttpModel.get(Api.GET_COINS_LIST, params, Api.GET_COINS_LIST_ID, this);
+    }
+
     @Override
     public void onSucceed(JSONObject object, int id, CommonalityModel commonality) {
+        if (object != null && commonality != null && !Utility.isEmpty(commonality.getStatusCode())) {
+            if (Constants.SUCESSCODE.equals(commonality.getStatusCode())) {
+                switch (id) {
+                    case Api.GET_COINS_LIST_ID:
 
+                        break;
+                }
+            }
+        }
+        stopProgressDialog();
     }
 
     @Override
     public void onFail() {
-
+        stopProgressDialog();
     }
 
     @Override
     public void onError(Exception e) {
-
+        stopProgressDialog();
     }
 }
