44acea0ab584db358b68862f3c2098f2bc969919
aq610428 <641897605@qq.com>
2020-11-05 15:01:03 +0800

    Merge branch 'master' of http://github.com/aq610428/chinaBox
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/jkabe/app/box/adapter/AsetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/AssetsAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/IncomeAdapter.java
    #	app/src/main/java/com/jkabe/app/box/adapter/TraverAdapter.java
    #	app/src/main/java/com/jkabe/app/box/base/BaseApplication.java
    #	app/src/main/java/com/jkabe/app/box/bean/UserInfo.java
    #	app/src/main/java/com/jkabe/app/box/box/AssetsActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/DrawActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/IncomeActivity.java
    #	app/src/main/java/com/jkabe/app/box/box/RechargeActivity.java
    #	app/src/main/java/com/jkabe/app/box/config/Api.java
    #	app/src/main/java/com/jkabe/app/box/config/okHttpModel.java
    #	app/src/main/java/com/jkabe/app/box/ui/ActivationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BindActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/BrandCarActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/InvitationActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LocationIndexActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/LoginActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/MainActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/RegisterActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/UserActivity.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/AssetsFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/CarFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MeFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MineFragment.java
    #	app/src/main/java/com/jkabe/app/box/ui/fragment/MiningFragmnt.java
    #	app/src/main/java/com/jkabe/app/box/util/Constants.java
    #	app/src/main/java/com/jkabe/app/box/util/FileManager.java
    #	app/src/main/java/com/jkabe/app/box/util/FileUtil.java
    #	app/src/main/java/com/jkabe/app/box/util/JsonParse.java
    #	app/src/main/java/com/jkabe/app/box/util/SaveUtils.java
    #	app/src/main/java/com/jkabe/app/box/util/SystemTools.java
    #	app/src/main/java/com/jkabe/app/box/weight/DialogUtils.java
    #	app/src/main/java/com/jkabe/app/box/weight/UpdateManager.java
    #	app/src/main/res/layout/activity_assets.xml
    #	app/src/main/res/layout/activity_bind.xml
    #	app/src/main/res/layout/activity_draw.xml
    #	app/src/main/res/layout/activity_income.xml
    #	app/src/main/res/layout/activity_location_map.xml
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_recharge.xml
    #	app/src/main/res/layout/custom_info_window.xml
    #	app/src/main/res/layout/fragment_assets.xml
    #	app/src/main/res/layout/fragment_me.xml
    #	app/src/main/res/layout/fragment_mining.xml
    #	app/src/main/res/layout/item_income.xml
    #	app/src/main/res/mipmap-xhdpi/icon_car.png
    #	app/src/main/res/mipmap-xxhdpi/icon_car.png
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    #	build.gradle
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/debug/R.jar
    #	library1/build/intermediates/compile_only_not_namespaced_r_class_jar/release/R.jar
    #	library1/build/intermediates/incremental/packageDebugResources/compile-file-map.properties
    #	library1/build/intermediates/incremental/packageReleaseResources/compile-file-map.properties
    #	library1/build/intermediates/manifest_merge_blame_file/debugAndroidTest/manifest-merger-blame-debug-androidTest-report.txt

--- app/src/main/java/com/jkabe/app/box/box/fragement/TabFragment5.java
+++ app/src/main/java/com/jkabe/app/box/box/fragement/TabFragment5.java
@@ -8,20 +8,29 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import com.jkabe.app.box.base.BaseFragment;
+import com.jkabe.app.box.bean.CommonalityModel;
+import com.jkabe.app.box.config.Api;
+import com.jkabe.app.box.config.NetWorkListener;
+import com.jkabe.app.box.config.okHttpModel;
+import com.jkabe.app.box.util.Constants;
+import com.jkabe.app.box.util.Md5Util;
+import com.jkabe.app.box.util.SaveUtils;
+import com.jkabe.app.box.util.Utility;
 import com.jkabe.box.R;
+import org.json.JSONObject;
+import java.util.Map;
 
 /**
  * @author: zt
  * @date: 2020/10/20
  * @name:TabFragment1
  */
-public class TabFragment5 extends BaseFragment {
+public class TabFragment5 extends BaseFragment implements NetWorkListener {
     private View rootView;
     private TextView text_tab1, text_tab2, text_tab3, text_tab4, text_tab5, text_tab6;
     private TextView text_tab_direct, text_tab1_direct, text_tab2_direct;
-    private TextView text_tab_usdt,text_tab1_usdt,text_tab1_box,text_tab2_box;
+    private TextView text_tab_usdt, text_tab1_usdt, text_tab1_box, text_tab2_box;
 
     @Nullable
     @Override
@@ -34,23 +43,64 @@ public class TabFragment5 extends BaseFragment {
     }
 
     private void initView() {
-        text_tab1=getView(rootView,R.id.text_tab1);
-        text_tab2=getView(rootView,R.id.text_tab2);
-        text_tab3=getView(rootView,R.id.text_tab3);
-        text_tab4=getView(rootView,R.id.text_tab4);
-        text_tab5=getView(rootView,R.id.text_tab5);
-        text_tab6=getView(rootView,R.id.text_tab6);
+        text_tab1 = getView(rootView, R.id.text_tab1);
+        text_tab2 = getView(rootView, R.id.text_tab2);
+        text_tab3 = getView(rootView, R.id.text_tab3);
+        text_tab4 = getView(rootView, R.id.text_tab4);
+        text_tab5 = getView(rootView, R.id.text_tab5);
+        text_tab6 = getView(rootView, R.id.text_tab6);
 
-        text_tab_direct=getView(rootView,R.id.text_tab_direct);
-        text_tab1_direct=getView(rootView,R.id.text_tab1_direct);
-        text_tab2_direct=getView(rootView,R.id.text_tab2_direct);
+        text_tab_direct = getView(rootView, R.id.text_tab_direct);
+        text_tab1_direct = getView(rootView, R.id.text_tab1_direct);
+        text_tab2_direct = getView(rootView, R.id.text_tab2_direct);
 
-
-        text_tab_usdt=getView(rootView,R.id.text_tab_usdt);
-        text_tab1_usdt=getView(rootView,R.id.text_tab1_usdt);
-        text_tab1_box=getView(rootView,R.id.text_tab1_box);
-        text_tab2_box=getView(rootView,R.id.text_tab2_box);
-
+        text_tab_usdt = getView(rootView, R.id.text_tab_usdt);
+        text_tab1_usdt = getView(rootView, R.id.text_tab1_usdt);
+        text_tab1_box = getView(rootView, R.id.text_tab1_box);
+        text_tab2_box = getView(rootView, R.id.text_tab2_box);
     }
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        query();
+    }
+
+    public void query() {
+        String sign = "memberid=" + SaveUtils.getSaveInfo().getId() + "&partnerid=" + Constants.PARTNERID + Constants.SECREKEY;
+        showProgressDialog(getActivity(), false);
+        Map<String, String> params = okHttpModel.getParams();
+        params.put("apptype", Constants.TYPE);
+        params.put("memberid", SaveUtils.getSaveInfo().getId());
+        params.put("partnerid", Constants.PARTNERID);
+        params.put("sign", Md5Util.encode(sign));
+        okHttpModel.get(Api.MINING_BAL_BOX, params, Api.GETRECORD_BOX_ID, this);
+    }
+
+
+    @Override
+    public void onSucceed(JSONObject object, int id, CommonalityModel commonality) {
+        if (object != null && commonality != null && !Utility.isEmpty(commonality.getStatusCode())) {
+            if (Constants.SUCESSCODE.equals(commonality.getStatusCode())) {
+                switch (id) {
+                    case Api.MINING_BOX_ID:
+
+                        break;
+
+                }
+            }
+        }
+        stopProgressDialog();
+    }
+
+    @Override
+    public void onFail() {
+        stopProgressDialog();
+    }
+
+    @Override
+    public void onError(Exception e) {
+        stopProgressDialog();
+    }
 }
